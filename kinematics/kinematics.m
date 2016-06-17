0e5a1d8d4430350436941440dab8d0a21b435f05
Pedro Guilherme S. Moreira <pedro.eletronica.unb@gmail.com>
2016-06-17 16:41:54 -0300

    Agora os parâmetros TOL, PASSO, e TIME_STEP podem ser alteradas fora do código

--- kinematics/kinematics.m
+++ kinematics/kinematics.m
@@ -6,8 +6,15 @@ warning off;
 format compact;
 format long;
 
-tol = 1;
-step = 1e-2;
+if ~exist('tol')
+	tol = 1;
+end
+if ~exist('passo')
+	passo = 1e-2;
+end
+if ~exist('time_step')
+	time_step = 1e-2;
+end
 
 L = [16; 5; 20];
 theta_0 = [pi/2; pi];
@@ -17,7 +24,7 @@ handle = draw_points(L, theta_0);
 
 [pos_x, pos_y] = ginput(1);
 joints = joint_position(L, theta);
-vel = step * ([pos_x, pos_y] - joints(5,:));
+vel = passo * ([pos_x, pos_y] - joints(5,:));
 vel = vel(:);
 figure(handle);
 pos = [pos_x, pos_y];
@@ -32,7 +39,7 @@ while 1
 		pos = [pos_x, pos_y];
 		continue;
 	end
-	pause(.01);
+	pause(time_step);
 	theta = theta + jacobiano_inverso(L, theta) * vel;
 	draw_points(L, theta, handle);
 	plot(pos_x, pos_y, 'r+'); hold on;
